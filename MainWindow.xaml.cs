7a1a7ad56113ed9e4c276a05cea915a95a055682
Guan Jiannan <guanjiannan@outlook.com>
2020-07-20 18:38:07 +0800

    侧边栏动态开关

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,11 +5,38 @@ namespace Dynamic_C_Notebook
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// 打开/关闭“文件”侧边栏
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FilesButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (FilesSidePanel.Visibility == Visibility.Visible)
+            {
+                FilesSidePanel.Visibility = Visibility.Collapsed;
+                FilesCloseButton.Visibility = Visibility.Visible;
+                FilesOpenButton.Visibility = Visibility.Collapsed;
+                SidePanelSplitter.Visibility = Visibility.Collapsed;
+                SidePanelAndCodeGrid.ColumnDefinitions[0].Width = new GridLength(0, GridUnitType.Pixel);
+                SidePanelAndCodeGrid.ColumnDefinitions[1].Width = new GridLength(0, GridUnitType.Pixel);
+            }
+            else
+            {
+                FilesSidePanel.Visibility = Visibility.Visible;
+                FilesCloseButton.Visibility = Visibility.Collapsed;
+                FilesOpenButton.Visibility = Visibility.Visible;
+                SidePanelSplitter.Visibility = Visibility.Visible;
+                SidePanelAndCodeGrid.ColumnDefinitions[0].Width = new GridLength(150, GridUnitType.Pixel);
+                SidePanelAndCodeGrid.ColumnDefinitions[1].Width = new GridLength(3, GridUnitType.Pixel);
+            }
         }
     }
 }
